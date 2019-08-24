174b8ffc52156e660771b94eabf79eb241d7b145
CJ Harries <cj@wizardsoftheweb.pro>
2019-08-24 01:33:07 -0500

    Add WorkingDirectory to base suite

--- common_test.go
+++ common_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"path/filepath"
+	"runtime"
 	"testing"
 
 	. "gopkg.in/check.v1"
@@ -10,6 +12,12 @@ func Test(t *testing.T) { TestingT(t) }
 
 type BaseSuite struct {
 	SharedErrorMessage string
+	WorkingDirectory   string
 }
 
 var _ = Suite(&BaseSuite{})
+
+func (s *BaseSuite) SetUpSuite(c *C) {
+	_, filename, _, _ := runtime.Caller(0)
+	s.WorkingDirectory = filepath.Dir(filename)
+}
