44b588b5766379f57fc929cc0f74672a3aa29847
CJ Harries <cj@wizardsoftheweb.pro>
2019-08-24 04:19:40 -0500

    Test RegexpSubmatchNamed

--- regexp_utils_test.go
+++ regexp_utils_test.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"regexp"
+	"strings"
+
+	. "gopkg.in/check.v1"
+)
+
 type RegexpUtilsSuite struct {
 	BaseSuite
+}
+
+var _ = Suite(&RegexpUtilsSuite{})
+
+func (s *RegexpUtilsSuite) TestRegexpSubmatchNamed(c *C) {
+	pattern := regexp.MustCompile(`^(?P<key>[^=]+)=(?P<value>.*)$`)
+	searchStringSlice := []string{"key", "value"}
+	results := RegexpSubmatchNamed(pattern, strings.Join(searchStringSlice, "="))
+	c.Assert(results["key"], Equals, searchStringSlice[0])
+	c.Assert(results["value"], Equals, searchStringSlice[1])
 }
