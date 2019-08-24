cbe26d1322fe21d8fea4250c1dabcf1146ea29bb
CJ Harries <cj@wizardsoftheweb.pro>
2019-08-24 04:11:45 -0500

    Add RegexpSubmatchNamed

---
+++ regex_utils.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"regexp"
+)
+
+func RegexpSubmatchNamed(regexpPattern regexp.Regexp, input string) map[string]string {
+	discovered := make(map[string]string)
+	match := regexpPattern.FindStringSubmatch(input)
+	keys := regexpPattern.SubexpNames()
+	for index, value := range match {
+		discovered[keys[index]] = value
+	}
+	return discovered
+}
