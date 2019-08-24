d2d6684807ccd405be8328f405bf67c1c937e99c
CJ Harries <cj@wizardsoftheweb.pro>
2019-08-24 04:28:30 -0500

    Build getSpecificMapKeys

--- regexp_utils.go
+++ regexp_utils.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"regexp"
+	"strings"
+
+	"github.com/pkg/errors"
 )
 
 func RegexpSubmatchNamed(regexpPattern *regexp.Regexp, input string) map[string]string {
@@ -12,4 +16,23 @@ func RegexpSubmatchNamed(regexpPattern *regexp.Regexp, input string) map[string]
 		discovered[keys[index]] = value
 	}
 	return discovered
+}
+
+func
+c.Assert(results["key"], Equals, searchStringSlice[0])(keysToGrab []string, input map[string]string) (map[string]string, error) {
+	found := make(map[string]string)
+	missing := []string{}
+	for _, key := range keysToGrab {
+		value, ok := input[key]
+		if ok {
+			found[key] = value
+		} else {
+			missing = append(missing, key)
+		}
+	}
+	var err error
+	if 0 >= len(missing) {
+		err = errors.New(fmt.Sprintf("Missing the following keys: %s", strings.Join(missing, ", ")))
+	}
+	return found, err
 }
