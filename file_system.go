569a31cddd83c826a5d7479532380641290e37cf
CJ Harries <cj@wizardsoftheweb.pro>
2019-08-24 02:13:15 -0500

    Use variable pathTidier for mock ability

--- file_system.go
+++ file_system.go
@@ -10,8 +10,10 @@ func tidyPath(pathComponents ...string) (string, error) {
 	return filepath.Abs(filepath.Join(pathComponents...))
 }
 
+var pathTidier = tidyPath
+
 func EnsureDirectoryExists(pathComponents ...string) error {
-	combinedPath, err := tidyPath(pathComponents...)
+	combinedPath, err := pathTidier(pathComponents...)
 	if nil == err {
 		err = os.MkdirAll(combinedPath, os.ModePerm)
 	}
@@ -19,7 +21,7 @@ func EnsureDirectoryExists(pathComponents ...string) error {
 }
 
 func LoadFile(pathComponents ...string) (string, error) {
-	combinedPath, err := tidyPath(pathComponents...)
+	combinedPath, err := pathTidier(pathComponents...)
 	if nil != err {
 		return "", err
 	}
