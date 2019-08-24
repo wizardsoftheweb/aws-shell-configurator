011a7cd6062db7f2a7f6db8a4e91b2afbb4302d7
CJ Harries <cj@wizardsoftheweb.pro>
2019-08-24 01:33:14 -0500

    Test tidyPath

--- file_system_test.go
+++ file_system_test.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+
+	. "gopkg.in/check.v1"
+)
+
+type FileSystemSuite struct {
+	BaseSuite
+}
+
+var _ = Suite(&FileSystemSuite{
+	BaseSuite{SharedErrorMessage: "shared file error"},
+})
+
+func (s *FileSystemSuite) TestTidyPath(c *C) {
+	var tidyPathData = [][]string{
+		{"/", "/"},
+		{"/some/dir", "/some", "dir"},
+		{fmt.Sprintf("%s/%s", s.WorkingDirectory, "some/dir"), "some", "dir"},
+	}
+	for _, value := range tidyPathData {
+		result, err := tidyPath(value[1:]...)
+		c.Assert(err, Not(ErrorMatches), "*")
+		c.Assert(result, Equals, value[0])
+	}
+}
