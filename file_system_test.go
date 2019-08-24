69a66dff5db05e4d658b49245ccf7fd85e11d43f
CJ Harries <cj@wizardsoftheweb.pro>
2019-08-24 02:13:23 -0500

    Test LoadFile

--- file_system_test.go
+++ file_system_test.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
 
+	"github.com/pkg/errors"
 	. "gopkg.in/check.v1"
 )
 
@@ -15,6 +17,10 @@ type FileSystemSuite struct {
 var _ = Suite(&FileSystemSuite{
 	BaseSuite{SharedErrorMessage: "shared file error"},
 })
+
+func (s *FileSystemSuite) TearDownTest(c *C) {
+	pathTidier = tidyPath
+}
 
 func (s *FileSystemSuite) TestTidyPath(c *C) {
 	var tidyPathData = [][]string{
@@ -46,4 +52,24 @@ func (s *FileSystemSuite) TestEnsureDirectoryExistsCreatesDirectories(c *C) {
 	c.Assert(err, IsNil)
 	_, err = os.Stat(fullPath)
 	c.Assert(os.IsNotExist(err), Equals, false)
+}
+
+func (s *FileSystemSuite) TestLoadFileThatDoesntExist(c *C) {
+	_, err := LoadFile(filepath.Join(s.WorkingDirectory, "random", "file"))
+	c.Assert(os.IsNotExist(err), Equals, true)
+}
+
+func (s *FileSystemSuite) TestLoadFileNonEmpty(c *C) {
+	_, filename, _, _ := runtime.Caller(0)
+	contents, err := LoadFile(filename)
+	c.Assert(err, IsNil)
+	c.Assert(contents, Not(Equals), "")
+}
+
+func (s *FileSystemSuite) TestLoadFilePathError(c *C) {
+	pathTidier = func(input ...string) (string, error) {
+		return "", errors.New(s.SharedErrorMessage)
+	}
+	_, err := LoadFile(s.WorkingDirectory)
+	c.Assert(err, ErrorMatches, s.SharedErrorMessage)
 }
